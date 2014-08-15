f7b15e558822d07cc0fa7299ec4104217c7f8dbe
Johnny-Malizia <johnny@hasoffers.com>
2014-08-14 20:13:37 -0700

    more crap is gone now

--- app/src/main/java/com/tune/applikitchen/LoginActivity.java
+++ app/src/main/java/com/tune/applikitchen/LoginActivity.java
@@ -4,11 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
+
 import android.os.AsyncTask;
 
 import android.os.Build;
@@ -18,7 +14,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
+
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,10 +22,6 @@ import android.widget.TextView;
 
 import com.tune.applikitchen.rest.Api;
 import com.tune.applikitchen.rest.responses.LoginResponse;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import ly.apps.android.rest.client.Callback;
 import ly.apps.android.rest.client.Response;
 import ly.apps.android.rest.client.RestClient;
@@ -43,13 +35,6 @@ import ly.apps.android.rest.client.RestServiceFactory;
  */
 public class LoginActivity extends Activity {
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
